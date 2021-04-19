f585a51b4b81038385fac22ece30fa157195fb50
Berna Köse <anreb_55@hotmail.com>
2021-04-20 01:24:36 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            //iş kuralları
+            if (product.CategoryId==1)
+            {
+                return new ErrorResult("Bu kategoriye ürün kabul edilmiyot");
+            }
+            else
+            {
+                _productDal.Add(product);
+                return new SuccessResult("Ürün Eklendi.");
+            }
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"Ürünler Listelendi");
+        }
+
+        public IDataResult<List<Product>> GetAllByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==categoryId),"Ürünler Listelendi");
+        }
+    }
+}
